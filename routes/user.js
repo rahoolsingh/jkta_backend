652c451aa42d3ed769c51cd59bd11afc5e3cf010
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-09-07 23:26:13 +0530

    RazorPay integrated

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,5 @@ const UserControll=require("../controller/form");
 const FormController=require("../controller/pass");
 Router.post('/register-user',UserControll.register);
 Router.get('/generatePass',FormController.passGenerator);
+Router.post('/verify-payment',UserControll.verifyPayment);
 module.exports=Router;
