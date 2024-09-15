41a637b76b66399ef47065e96efe9396c2cd513f
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-09-15 12:06:33 +0530

    Refactor user.js to add a greeting route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,12 @@ const express=require('express');
 const Router =express.Router()
 const UserControll=require("../controller/form");
 const CoachController=require("../controller/coachForm");
+
+
+Router.get('/',(req,res)=>{
+    res.send("Khamma Ghani Sa");
+})
+
 Router.post('/register-user',UserControll.register);
 
 Router.post('/verify-payment',UserControll.verifyPayment);
